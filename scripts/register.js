2194b9ed9e8ad095cece64a52e0f368595fc412e
Davi Melo <davicosta01082008@gmail.com>
2024-11-22 20:34:26 -0300

    add login page

---
+++ scripts/register.js
@@ -0,0 +1,20 @@
+const dateOfBirth = document.querySelector('#date-of-birth')
+console.log(dateOfBirth)
+
+dateOfBirth.addEventListener('keypress', (e) => {
+  if (e.keyCode < 47 || e.keyCode > 57) {
+    e.preventDefault()
+  }
+
+  const len = dateOfBirth.value.length
+    
+  if (len !== 1 || len !== 3) {
+    if (e.keyCode == 47) {
+      e.preventDefault()
+    }
+  }
+    
+  if(len === 2 || len === 5) {
+    dateOfBirth.value += '/'
+  }
+})
