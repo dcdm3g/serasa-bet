300e5001da3c1e84f6dcd57fffa579b023b32405
Davi Melo <davicosta01082008@gmail.com>
2024-11-29 04:35:50 -0300

    feat: add events search

--- static/scripts/events.js
+++ static/scripts/events.js
@@ -1,5 +1,6 @@
 const input = document.querySelector('#search-input')
 const form = document.querySelector('#search-form')
+const list = document.querySelector('#event-list')
 
 const searchParams = new URLSearchParams(window.location.search)
 const search = searchParams.get('search') ?? ''
@@ -8,21 +9,50 @@ input.value = search
 
 function renderEventCard({ id, title, description, odds_value, bet_count }) {
   return `
-    <a class="no-decoration" href="/event?id=${id}">
-      <li class="scrollable-section__item">
+    <li class="scrollable-section__item">
+      <a class="no-decoration" href="/event?id=${id}">
         <div class="card event-card">
           <div class="card__title">${title}</div>
           <div class="card__description">${description}</div>
 
           <div class="event-card__info">
-            <div class="event-card__info-text">${parseFloat(odds_value).toFixed(2)}</div>
-            <div class="event-card__separator"></div>
-            <div class="event-card__info-text">${bet_count} bets</div>
+            <div class="event-card__info-text">
+              R$ ${parseFloat(odds_value).toFixed(2)}
+            </div>
           </div>
         </div>
-      </li>
-    </a>
+      </a>
+    </li>
   `
 }
 
-form.addEventListener('submi')
+function handleSearch(search) {
+  fetch('http://localhost:5000/events?search=' + search, {
+    method: 'GET',
+    credentials: 'include'
+  })
+  .then((response) => {
+    if (response.status !== 200) {
+      alert('Uh oh! There was an error on our end. Please try again later.')
+      return
+    }
+
+    return response.json()
+  }).then((data) => {
+    list.innerHTML = data.events.reduce((acc, cur) => {
+      return acc + renderEventCard(cur)
+    }, '')
+  })
+}
+
+handleSearch(search)
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  const searchParams = new URLSearchParams(window.location.search)
+  searchParams.set('search', input.value)
+  window.history.replaceState(null, '', '?' + searchParams.toString())
+
+  handleSearch(input.value)
+})
